0bc212e5d63f42705266fc0ac5fc4970c4899c21
sandeep gangwar <sandeep.gangwar@silicus.com>
2017-08-26 17:54:32 +0530

    Added bootstrap framework

--- app/products/productListCtrl.js
+++ app/products/productListCtrl.js
@@ -1,7 +1,31 @@
 (function(){
     'use strict';
     angular.module('productManagement')
-    .controller('ProductListController',[function(){
-        
-    }]);
+    .controller('ProductListController',[ProductListController]);
+    
+    function ProductListController(){
+        var vm = this;
+        vm.products=[{
+            "productId":1,
+            "productName":"A",
+            "productCode":"B",
+            "releaseDate":"12/01/2014",
+            "price":12.90,
+            "imageUrl":"https://www.logomaker.com/wp-content/uploads/2015/06/Logo-Samples2-91-min.jpg"
+          },
+          {
+            "productId":1,
+            "productName":"A",
+            "productCode":"B",
+            "releaseDate":"12/01/2015",
+            "price":12.90,
+            "imageUrl":"https://www.logomaker.com/wp-content/uploads/2015/06/Logo-Samples2-91-min.jpg"
+          }
+          ];
+
+          vm.showImage =false;
+          vm.toogleImage = function(){
+              vm.showImage =!vm.showImage;
+          }
+    }
 }());
