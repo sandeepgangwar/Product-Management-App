52cf1134060b6c518744ecfcaeb29cb5c5fab7cc
sandeep gangwar <sandeep.gangwar@silicus.com>
2017-08-26 23:45:00 +0530

    Added routing

--- app/app.js
+++ app/app.js
@@ -1,6 +1,15 @@
 (function(){
     'use strict';   
-    var app =angular.module('productManagement',['common.services','productResourceMock']);
+    var app =angular.module('productManagement',['common.services', 
+    'productResourceMock',
+    'ui.router']);
+    app.config(['$stateProvider',function($stateProvider){
+        $stateProvider.state('productList',{
+            url:'/products',
+            templateUrl:'/app/products/productListView.html',
+            controller:'ProductListController as vm'
+        })
+    }])
 }());
 
 
--- common/services/productResourceMock.js
+++ common/services/productResourceMock.js
@@ -37,7 +37,37 @@
               ];
 
               var produuctUrl = "/api/products";
-
               $httpBackend.whenGET(produuctUrl).respond(products);
-    })
+              var editingRegex  = new RegExp(produuctUrl + "/[0-9][0-9]*",'');
+              $httpBackend.whenGET(editingRegex).respond(function(method,url,data){
+                var product = {productId:0};
+                var parameters = url.split('/');
+                var length = parameters.length;
+                var id = parameters[length-1];
+                if(id>0){
+                    product = products.filter((item)=> item.productId == id)[0];
+                }
+                return [200,product,{}];
+              });
+
+              $httpBackend.whenPOST(produuctUrl).respond(function(method,url,data){
+                  var product = angular.fromJSON(data);
+                  if(!product.productId){
+                      product.productId = products[products.length -1].productId + 1;
+                      products.push(product);
+                  }else{
+                      for (var index = 0; index < products.length; index++) {
+                          if(products[index].productId === product.productId)
+                          {
+                              products[index]= product;
+                              break;
+                          }
+                      }
+                  }
+
+                  return [200,product,{}];
+              });
+
+             $httpBackend.whenGET(/app/).passThrough();
+    });
 }());
